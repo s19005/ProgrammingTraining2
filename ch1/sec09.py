ff91cecf88acda610607bac549af4709783720c1
yuto <s19005@std.it-college.ac.jp>
2020-06-29 10:31:19 +0900

    Typoglycemia

---
+++ ch1/sec09.py
@@ -0,0 +1,15 @@
+import random
+
+sentence = 'I couldn’t believe that I could actually understand what I was reading : the phenomenal power of the human mind .'
+li1 = sentence.split()
+li2 = []
+
+for word in li1:
+    if len(word) <= 4:
+        li2 += [word]
+    else:
+        li3 = list(word[1:len(word) - 1])
+        random.shuffle(li3)
+        li2 += [word[0] + ''.join(li3) + word[len(word) - 1]]
+
+print(li2)
